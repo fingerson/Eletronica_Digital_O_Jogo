f6dd009beaf451272d4ead0c3378432c3fc34070
fingerson <henfinzim@gmail.com>
2018-07-18 23:39:11 -0300

    Added lamp and gnome sprites, gnome animations

---
+++ Scripts/Player.gd
@@ -0,0 +1,44 @@
+extends KinematicBody2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+var SPEED = 50
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+	
+func set_animation (anim):
+	if $AnimationPlayer.get_current_animation() != anim:
+		$AnimationPlayer.play(anim)
+	
+func play():
+	if not $AnimationPlayer.is_playing():
+		$AnimationPlayer.play()
+
+func stop():
+	if $AnimationPlayer.is_playing():
+		$AnimationPlayer.stop()
+
+func _process(delta):
+	# Called every frame. Delta is time since last frame.
+	# Update game logic here.
+	var direction = Vector2(0,0)
+	if Input.is_action_pressed("ui_up"):
+		set_animation("walking_up")
+		direction.y = -1 * abs(scale.y)
+	elif Input.is_action_pressed("ui_down"):
+		set_animation("walking_down")
+		direction.y = 1 * abs(scale.y)
+	elif Input.is_action_pressed("ui_left"):
+		set_animation("walking_left")
+		direction.x = -1 * abs(scale.x)
+	elif Input.is_action_pressed("ui_right"):
+		set_animation("walking_right")
+		direction.x = 1 * abs(scale.x)
+	else:
+		stop()
+	move_and_slide(direction * SPEED)
+	pass
--- Scripts/Title.gd
+++ Scripts/Title.gd
@@ -7,14 +7,14 @@ extends Node2D
 signal out_mode(mode)
 
 func _ready():
-	$New_game.connect("button_down",self,"on_ng")
-	$Exit.connect("button_down",self,"on_quit")
+	$New_game.connect("button_down",self,"_on_ng")
+	$Exit.connect("button_down",self,"_on_quit")
 	pass
 
-func on_ng():
+func _on_ng():
 	emit_signal("out_mode","ng")
 	
-func on_quit():
+func _on_quit():
 	emit_signal("out_mode","quit")
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
